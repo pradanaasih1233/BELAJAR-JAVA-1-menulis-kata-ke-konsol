2a980928511649f5781c805b54e0d2227570e093
pradanaasih1233 <85855470+pradanaasih1233@users.noreply.github.com>
2022-07-19 15:37:06 +0700

    Ayo belajar java..!!!
    Tutorial pertama dengan bahasa indonesia....!!!

---
+++ src/com/belajar1/main.java
@@ -0,0 +1,23 @@
+// oraganisai folder
+package com.belajar1;
+
+// class dalam java
+public class main {
+
+    // fungsi utama dalam program
+    public static void main(String[] args){
+
+        System.out.println("MENULIS KATA KEDALAM CONSOL");
+
+        // cara menampilkan kata/kalimat di consol
+        // 1. dapat dengan System.out.println(); dengan enter
+        System.out.println("1. ini adalah perogam pertama saya dgn enter");
+        System.out.println(); // ini sebagai enter
+
+        // 2. dapat dengan System.out.print(); tanpa enter
+        System.out.print("2. ini adalah perogam pertama saya tanpa");
+        System.out.print(" enter");
+        System.out.println("ini adalah enter");
+
+    }
+}
